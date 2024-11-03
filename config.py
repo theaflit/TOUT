5faf9d55c0f52d993c2b96e0120b7cf397314878
thealfit <147757914+theaflit@users.noreply.github.com>
2024-11-03 23:33:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    token_telegram: str = '8062147761:AAGth2Rz9nI0EIe6X030malr2mJzWz7gMI4'
-    token_yadisk: str = 'y0_AgAAAAA1ETRtAAysmgAAAAEWAnRGAACtIaLGgfFMwp6sh7MNBMGE4fIohQ'
-    admin_id: int = 829836737
+    token_telegram: str = ''
+    token_yadisk: str = ''
+    admin_id: int = 11111111
     model_path: str = "t5-small-finetuned-xsum"
     tokenizer_path: str = "t5-small-finetuned-xsum"
     dir_name: str = "/Проделанная работа"
